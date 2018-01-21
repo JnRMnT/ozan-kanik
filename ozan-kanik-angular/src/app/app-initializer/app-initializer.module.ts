7d319efd71dd49a5a99c30cebda9f9c371c43765
Ozan KANIK <l1110018@std.yildiz.edu.tr>
2018-01-21 14:13:10 +0300

    Application startup error handling is implemented

--- ozan-kanik-angular/src/app/app-initializer/app-initializer.module.ts
+++ ozan-kanik-angular/src/app/app-initializer/app-initializer.module.ts
@@ -20,7 +20,18 @@ import { CommonModule } from '@angular/common';
 export class AppInitializerModule { }
 
 export function initializeResources(httpService: HttpService, translateService: TranslateService): () => Promise<any> {
+  
   return (): Promise<any> => {
-    return translateService.initialize();
-  }
+    return new Promise((resolve, reject) => {
+      translateService.initialize().then(() => {
+        resolve();
+      }, (error) => {
+        const errorTitle = translateService.getLocalResource("Title.Error");
+        const errorMessage = translateService.getLocalResource("Exception.Common");
+        const simpleError = '<div class="custom-error alert alert-danger"><strong>' + errorTitle + '</strong> ' + errorMessage + '</div>';
+        $("app-root .loader-overlay").html(simpleError);
+        reject();
+      });
+    });
+  };
 }
--- ozan-kanik-angular/src/app/translate.service.ts
+++ ozan-kanik-angular/src/app/translate.service.ts
@@ -4,6 +4,16 @@ import { Resource } from './response-models/resource';
 
 @Injectable()
 export class TranslateService {
+  private localResources: any = {
+    "en-US": {
+      "Title.Error": "Error",
+      "Exception.Common": "	We are not able to process your request at the moment, please try again later."
+    },
+    "tr-TR": {
+      "Title.Error": "Hata",
+      "Exception.Common": "İsteğinizi şu anda gerçekleştiremiyoruz, lütfen daha sonra tekrar deneyiniz."
+    }
+  };
 
   constructor(private httpService: HttpService) {
   }
@@ -11,6 +21,7 @@ export class TranslateService {
   public initialize(): Promise<any> {
     const me = this;
     return new Promise((resolve, reject) => {
+      me.use();
       me.httpService.get("resources").subscribe((resources: Resource[]) => {
         if (resources) {
           me.allResources = resources;
@@ -25,15 +36,21 @@ export class TranslateService {
   public use(cultureCode?: string) {
     const me = this;
     if (!cultureCode) {
-      cultureCode = "en-US";
+      if (window.location.hostname.endsWith(".tr")) {
+        cultureCode = "tr-TR";
+      } else {
+        cultureCode = "en-US";
+      }
     }
 
     this.resources = {};
-    this.allResources.filter((item) => {
-      return item.culture == cultureCode;
-    }).forEach((resource: Resource) => {
-      me.resources[resource.key] = resource.value;
+    if (this.allResources && this.allResources.length > 0) {
+      this.allResources.filter((item) => {
+        return item.culture == cultureCode;
+      }).forEach((resource: Resource) => {
+        me.resources[resource.key] = resource.value;
       });
+    }
 
     this.activeCulture = cultureCode;
   }
@@ -43,6 +60,10 @@ export class TranslateService {
       return "";
     }
     return this.resources[key];
+  }
+
+  public getLocalResource(key: string): string {
+    return this.localResources[this.activeCulture][key];
   }
 
   public resources: any;
