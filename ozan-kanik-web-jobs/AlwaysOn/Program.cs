3a8876203a1c641bee0c3dc23b40e6eedd2b4350
Ozan KANIK <l1110018@std.yildiz.edu.tr>
2019-07-25 21:50:37 +0200

    AlwaysOn WebJob Implementation

---
+++ ozan-kanik-web-jobs/AlwaysOn/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlwaysOn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = "https://" + ConfigurationManager.AppSettings.Get("WEBSITE_SITE_NAME") + ".azurewebsites.net";
+                Console.WriteLine("Getting " + url + "...");
+                Task.Run(async () =>
+                {
+                    HttpResponseMessage responseMessage = await client.GetAsync(url);
+                    Console.WriteLine(responseMessage.StatusCode);
+                }).Wait();
+            }
+        }
+    }
+}
