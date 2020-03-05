63b430dab575a93567aa539fb168a8fad65a4551
Ozan KANIK <l1110018@std.yildiz.edu.tr>
2020-03-05 01:34:58 +0100

    Angular update initial commit

--- ozan-kanik-angular/src/app/http.service.ts
+++ ozan-kanik-angular/src/app/http.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PreferencesService } from './preferences.service';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs';
 import { LoadingService } from './loading.service';
 
 @Injectable()
